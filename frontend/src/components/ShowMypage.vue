f1496f547c13a305192719e00f2f59f623dae1ae
jisunchung <65282581+jisunchung@users.noreply.github.com>
2021-09-18 12:50:49 +0900

    commit

--- frontend/src/components/ShowMypage.vue
+++ frontend/src/components/ShowMypage.vue
@@ -184,7 +184,7 @@ export default {
     },
     watch:{
         userStoredCities :function(){
-            this.getStoredCitiesDetail()
+            this.getStoredCitiesDetail() 
         }
     }
     
